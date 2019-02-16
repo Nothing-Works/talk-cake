2ce1babde78665cdfa0841a62c1f31f6cc34be94
AndySong <andylauszp@gmail.com>
2019-02-16 18:50:06 +1300

    some test refactoring

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-02-16 04:21:39.
+    * Generated on 2019-02-16 05:21:06.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- tests/Feature/NotificationTest.php
+++ tests/Feature/NotificationTest.php
@@ -4,16 +4,22 @@ namespace Tests\Feature;
 
 use App\Thread;
 use App\User;
+use Illuminate\Notifications\DatabaseNotification;
 use Tests\TestCase;
 
 class NotificationTest extends TestCase
 {
-    public function test_a_notification_is_prepared_when_a_subscribed_thread_receives_a_new_reply_not_by_current_user()
+    protected function setUp()
     {
+        parent::setUp();
+
         $user = factory(User::class)->create();
 
         $this->actingAs($user);
+    }
 
+    public function test_a_notification_is_prepared_when_a_subscribed_thread_receives_a_new_reply_not_by_current_user()
+    {
         $thread = factory(Thread::class)->create()->subscribe();
 
         $this->assertCount(1, $thread->subscriptions);
@@ -37,41 +43,19 @@ class NotificationTest extends TestCase
 
     public function test_a_user_can_mark_a_notification_as_read()
     {
-        $user = factory(User::class)->create();
-
-        $this->actingAs($user);
-
-        $thread = factory(Thread::class)->create()->subscribe();
-
-        $thread->addReply([
-            'user_id' => factory(User::class)->create()->id,
-            'body' => 'Some reply here',
-        ]);
+        factory(DatabaseNotification::class)->create();
 
         $this->assertCount(1, auth()->user()->unreadNotifications);
 
-        $notificationId = auth()->user()->unreadNotifications->first()->id;
-
-        $this->delete('/profiles/'.auth()->user()->name.'/notifications/'.$notificationId);
+        $this->delete('/profiles/'.auth()->user()->name.'/notifications/'. auth()->user()->unreadNotifications->first()->id);
 
         $this->assertCount(0, auth()->user()->fresh()->unreadNotifications);
     }
 
     public function test_a_user_can_fetch_their_unread_notifications()
     {
-        $user = factory(User::class)->create();
+        factory(DatabaseNotification::class)->create();
 
-        $this->actingAs($user);
-
-        $thread = factory(Thread::class)->create()->subscribe();
-
-        $thread->addReply([
-            'user_id' => factory(User::class)->create()->id,
-            'body' => 'Some reply here',
-        ]);
-
-        $response = $this->getJson('/profiles/'.auth()->user()->name.'/notifications')->json();
-
-        $this->assertCount(1, $response);
+        $this->assertCount(1, $this->getJson('/profiles/'.auth()->user()->name.'/notifications')->json());
     }
 }
