f503ce11ddba02a2bb1ec7a716247b2b4c9adc02
AndySong <andylauszp@gmail.com>
2019-04-08 16:05:30 +1200

    wip

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -4,82 +4,81 @@ namespace App\Http\Controllers;
 
 use App\Activity;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ActivityController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
-        //
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
-        //
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return Response
      */
     public function store(Request $request)
     {
-        //
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Activity  $activity
-     * @return \Illuminate\Http\Response
+     * @param \App\Activity $activity
+     *
+     * @return Response
      */
     public function show(Activity $activity)
     {
-        //
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Activity  $activity
-     * @return \Illuminate\Http\Response
+     * @param \App\Activity $activity
+     *
+     * @return Response
      */
     public function edit(Activity $activity)
     {
-        //
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Activity  $activity
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Activity            $activity
+     *
+     * @return Response
      */
     public function update(Request $request, Activity $activity)
     {
-        //
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Activity  $activity
-     * @return \Illuminate\Http\Response
+     * @param \App\Activity $activity
+     *
+     * @return Response
      */
     public function destroy(Activity $activity)
     {
-        //
     }
 }
