f85032f08aa171d7d6ac1cd6cf4a06e94ad07632
AndySong <andylauszp@gmail.com>
2019-04-09 12:21:43 +1200

    wip

--- resources/js/components/NewReply.vue
+++ resources/js/components/NewReply.vue
@@ -3,6 +3,7 @@
         <div class="field">
             <div class="control">
                 <textarea
+                    id="input"
                     v-model="body"
                     name="body"
                     class="textarea"
@@ -20,6 +21,7 @@
 </template>
 
 <script>
+import Tribute from 'tributejs'
 export default {
     name: 'NewReply',
     props: {
@@ -38,6 +40,43 @@ export default {
             return window.shared.signedIn
         }
     },
+
+    mounted() {
+        const tribute = new Tribute({
+            trigger: '@',
+            iframe: null,
+            selectClass: 'highlight',
+            selectTemplate: function(item) {
+                return '@' + item.original.value
+            },
+            menuItemTemplate: function(item) {
+                return item.string
+            },
+            noMatchTemplate: null,
+            menuContainer: document.body,
+            lookup: 'key',
+            fillAttr: 'value',
+            values: [
+                { key: 'Phil Heartman', value: 'pheartman' },
+                { key: 'Gordon Ramsey', value: 'gramsey' },
+                { key: 'Gordon Ramsey', value: 'gramsey' },
+                { key: 'Gordon Ramsey', value: 'gramsey' },
+                { key: 'Gordon Ramsey', value: 'gramsey' },
+                { key: 'Gordon Ramsey', value: 'gramsey' }
+            ],
+            requireLeadingSpace: true,
+            allowSpaces: false,
+            replaceTextSuffix: '\n',
+            positionMenu: true,
+            spaceSelectsMatch: false,
+            autocompleteMode: false,
+            searchOpts: {
+                pre: '<span>',
+                post: '</span>'
+            }
+        })
+        tribute.attach(document.getElementById('input'))
+    },
     methods: {
         submit() {
             axios
@@ -53,3 +92,42 @@ export default {
     }
 }
 </script>
+
+<style>
+/*@import '~tributejs/dist/tribute.css';*/
+
+.tribute-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: auto;
+    max-height: 300px;
+    max-width: 500px;
+    overflow: auto;
+    display: block;
+    z-index: 999999;
+}
+.tribute-container ul {
+    margin: 0;
+    margin-top: 2px;
+    padding: 0;
+    list-style: none;
+    background: #efefef;
+}
+.tribute-container li {
+    padding: 5px 5px;
+    cursor: pointer;
+}
+.tribute-container li.highlight {
+    background: #ddd;
+}
+.tribute-container li span {
+    font-weight: bold;
+}
+.tribute-container li.no-match {
+    cursor: default;
+}
+.tribute-container .menu-highlighted {
+    font-weight: bold;
+}
+</style>
