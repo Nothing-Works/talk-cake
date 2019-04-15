c8ac782b525ab97a6efe8a833a8edda12173b42a
AndySong <andylauszp@gmail.com>
2019-04-15 16:03:41 +1200

    redis refactoring

--- tests/Feature/TrendingThreadsTest.php
+++ tests/Feature/TrendingThreadsTest.php
@@ -3,18 +3,31 @@
 namespace Tests\Feature;
 
 use App\Thread;
-use Illuminate\Support\Facades\Redis;
+use App\Trending;
 use Tests\TestCase;
 
 class TrendingThreadsTest extends TestCase
 {
+    /**
+     * @var Trending
+     */
+    protected $trending;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->trending = new Trending();
+
+        $this->trending->reset();
+    }
+
     public function test_it_increment_a_threads_score_each_time_it_is_read()
     {
-        $this->assertCount(0, Redis::zrevrange('trending_threads', 0, -1));
+        $this->assertCount(0, $this->trending->get());
         $thread = factory(Thread::class)->create();
         $this->call('GET', $thread->path());
-        $trending = Redis::zrevrange('trending_threads', 0, -1);
+        $trending = $this->trending->get();
         $this->assertCount(1, $trending);
-        $this->assertEquals($thread->title, json_decode($trending[0])->title);
+        $this->assertEquals($thread->title, $trending[0]->title);
     }
 }
