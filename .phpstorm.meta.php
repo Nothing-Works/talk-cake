0fa413bd0ed2b8f7040e99f712c5ba58e4a06d6e
AndySong <andylauszp@gmail.com>
2019-04-23 10:32:23 +1200

    update packages

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-04-16 04:36:23.
+    * Generated on 2019-04-22 22:29:25.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.28 on 2019-04-16 04:36:22.
+ * Generated for Laravel 5.7.28 on 2019-04-22 22:29:24.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
