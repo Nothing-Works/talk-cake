49c19b761b18b1373e14eb5cbf5fbde8b068e954
AndySong <andylauszp@gmail.com>
2019-05-30 17:07:01 +1200

    update package

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.28 on 2019-05-24 04:46:55.
+ * Generated for Laravel 5.7.28 on 2019-05-30 05:02:05.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
