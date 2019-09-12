3120d7f87266e63d9f1a96a07f6ac8814cb891ef
AndySong <andylauszp@gmail.com>
2019-09-12 14:23:43 +1200

    wip

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.28 on 2019-09-11 04:10:16.
+ * Generated for Laravel 5.7.28 on 2019-09-12 01:45:04.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
